03df2239fcd3d40f933254ab5b2cb032071a8737
franz-314 <franz.dy123@gmail.com>
2019-07-21 16:13:45 -0400

    servicios

--- src/app/services/quotes.service.ts
+++ src/app/services/quotes.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { APP_CONSTANTS } from '../../../configs/app-constants.config';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class QuotesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getQuotes(): Observable<any> {
+    return this.http.get(APP_CONSTANTS.quotes.stormconsultancy.co.uk/random.json);
+  }
 }
